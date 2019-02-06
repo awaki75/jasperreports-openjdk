e7ef6023a675d879b350b4cf39d55b70072a6163
awaki75 <awaki.33s@gmail.com>
2019-02-06 23:57:54 +0900

    Write PDF with jasperreports

--- src/main/java/io/github/awaki75/jasperreportsopenjdk/Main.java
+++ src/main/java/io/github/awaki75/jasperreportsopenjdk/Main.java
@@ -1,8 +1,29 @@
 package io.github.awaki75.jasperreportsopenjdk;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+
+import net.sf.jasperreports.engine.JREmptyDataSource;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello, world.");
+    public static void main(String[] args) throws Exception {
+        new Main().run();
+    }
+
+    private void run() throws IOException, JRException {
+        try (InputStream inputStream = this.getClass().getResourceAsStream("/hello.jrxml")) {
+            JasperReport jasperReport = JasperCompileManager.compileReport(inputStream);
+            JasperPrint jasperPrint = JasperFillManager.fillReport(
+                    jasperReport, new HashMap<String, Object>(), new JREmptyDataSource());
+            JasperExportManager.exportReportToPdfFile(jasperPrint, "hello.pdf");
+        }
     }
 }
